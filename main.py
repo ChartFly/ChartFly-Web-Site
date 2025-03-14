4606cd10e9543a4cbd5d026a03a986d3721b5f5a
ChartFly <f.moellers@comcast.net>
2025-03-13 21:43:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 # Tell FastAPI to serve the files in the 'static' folder
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
+from fastapi.staticfiles import StaticFiles
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
